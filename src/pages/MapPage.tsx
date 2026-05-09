abff2241e8ae930af618a159074ea3037fb30b35
Suravi Lokhande <lokhandesuravi@gmail.com>
2026-05-09 15:00:43 -0700

    Add interactive Davis map

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,9 +1,25 @@
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet"
+
+const center: [number, number] = [38.5449, -121.7405]
+
+function SetView() {
+  const map = useMap()
+  map.setView(center, 15)
+  return null
+}
+
 export default function MapPage() {
   return (
-    <div className="min-h-screen bg-black text-white">
-      <h1 className="p-6 text-4xl font-bold">
-        Map Page
-      </h1>
+    <div className="h-screen w-screen">
+      <MapContainer className="h-full w-full">
+        <SetView />
+
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        <Marker position={center}>
+          <Popup>UC Davis</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
