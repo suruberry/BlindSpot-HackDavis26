afcb3a38772b69230ed4cf077c342bbf3d848a63
Suravi Lokhande <lokhandesuravi@gmail.com>
2026-05-09 19:45:44 -0700

    Updated maps

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -89,7 +89,10 @@ export default function MapPage() {
       <div className="relative h-screen w-full max-w-md overflow-hidden">
         <MapContainer className="h-full w-full" center={center} zoom={14}>
           <SetView />
-          <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" />
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          />
 
           {reports.map((report) => (
             <Marker
